246ca94204abb773a145706eca22058e9cf19b79
Aral Balkan (LabTop) <mail+elementary-labtop@ar.al>
2021-07-23 16:14:23 +0100

    Fixes #1: HMR cannot connect, polls for restart

--- server/index.js
+++ server/index.js
@@ -3,6 +3,10 @@ const { createPageRender } = require('vite-plugin-ssr')
 const https = require('@small-tech/https')
 const hostname = require('@small-tech/cross-platform-hostname')
 
+const path = require('path')
+const fs = require('fs')
+const os = require('os')
+
 const isProduction = process.env.NODE_ENV === 'production'
 const root = `${__dirname}/..`
 
@@ -15,10 +19,22 @@ async function startServer() {
   if (isProduction) {
     app.use(express.static(`${root}/dist/client`, { index: false }))
   } else {
+    // Set  Vite server up as middleware and configure HMR to use
+    // same development-time certificate as used by @small-tech/https.
+    const certificateDirectory = path.join(os.homedir(), '.small-tech.org', 'https', 'local')
+    const cert = fs.readFileSync(path.join(certificateDirectory, 'localhost.pem'), 'utf-8')
+    const key = fs.readFileSync(path.join(certificateDirectory, 'localhost-key.pem'), 'utf-8')
+
     const vite = require('vite')
     viteDevServer = await vite.createServer({
       root,
-      server: { middlewareMode: true }
+      server: {
+        middlewareMode: true,
+        https: {
+          cert,
+          key
+        }
+      },
     })
     app.use(viteDevServer.middlewares)
   }
